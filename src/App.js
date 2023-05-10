bd4c3a1bd76e485ab882e26716abf3873e57eba3
chandukotari <pandra@ualr.edu>
2023-05-09 22:12:02 -0500

    Done Assignment-08

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import React, {Component} from "react";
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
-import Main from './components/MainComponent';
-import { BrowserRouter } from 'react-router-dom';
+import "./App.css";
+import React, { Component } from "react";
+import Main from "./components/MainComponent";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { ConfigureStore } from "./redux/configureStore";
 
-
+const store = ConfigureStore();
 class App extends Component {
-  /*constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }*/
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
-    )
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+
+    );
     /* return (
       <div className="App">
         <Navbar dark color="primary">
@@ -35,6 +30,5 @@ class App extends Component {
     ); */
   }
 }
-
 
 export default App;
